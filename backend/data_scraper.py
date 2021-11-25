9e58d604b04ef43ff06aa79b96b3e0caca9298e0
Zayan S. Khan <zayan_k@icloud.com>
2021-11-25 00:16:31 +0000

    MIC-1 refactoring

--- backend/data_scraper.py
+++ backend/data_scraper.py
@@ -4,12 +4,11 @@ from sources import *
 
 class DataScraper:
 
-    def prepare_nasdaq_data(self):
-        raw_nasdaq_data_df = self.get_raw_nasdaq_data()
-        raw_nasdaq_data_df.set_axis(['TICKER', 'PRICE', 'CHG', 'PRICE_CHANGE', 'RATING', 'VOLUME', 'VOLUME*PRICE',
-                                     'MARKET CAP', 'P/E', 'EPS', 'NUMBER OF EMPLOYEES', 'SECTOR'], axis=1,
-                                    inplace=True)
-        return raw_nasdaq_data_df
+    @staticmethod
+    def prepare_trading_view_data(scraped_trading_view_df):
+        return scraped_trading_view_df.set_axis(['TICKER', 'PRICE', 'CHG', 'PRICE_CHANGE', 'RATING', 'VOLUME',
+                                                'VOLUME*PRICE', 'MARKET CAP', 'P/E', 'EPS', 'NUMBER OF EMPLOYEES',
+                                                 'SECTOR'], axis=1, inplace=True)
 
     @staticmethod
     def get_raw_nasdaq_data():
@@ -18,5 +17,5 @@ class DataScraper:
 
 if __name__ == '__main__':
     d = DataScraper()
-    df = d.prepare_nasdaq_data()
+    df = d.prepare_trading_view_data(d.get_raw_nasdaq_data())
     print('debug breakpoint')

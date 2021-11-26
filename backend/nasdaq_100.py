947361fe84182666bf4f157fd0084e19b9b16057
Zayan S. Khan <zayan_k@icloud.com>
2021-11-26 00:52:35 +0000

    MIC-15 sectors df's available

--- backend/nasdaq_100.py
+++ backend/nasdaq_100.py
@@ -8,11 +8,31 @@ class Nasdaq100:
     def load_nasdaq_100_data():
         return pd.read_csv(DB_PATH + NASDAQ, index_col=0)
 
+    def get_nasdaq_100_sectors_df(self):
+        nasdaq_100_df = self.load_nasdaq_100_data()
+        sector_list = self.get_sector_list(nasdaq_100_df)
+        nasdaq_100_obj = self.get_nasdaq_sectors_obj(nasdaq_100_df)
+        sector_df_list = []
+        for sector in sector_list:
+            sector_df = self.get_df_for_sector(nasdaq_100_obj, sector)
+            sector_df_list.append(sector_df)
+        return sector_df_list
+
+    @staticmethod
+    def get_df_for_sector(obj, sector_name):
+        return obj.get_group(sector_name)
+
+    @staticmethod
+    def get_nasdaq_sectors_obj(df):
+        df_obj = df.groupby('SECTOR')
+        return df_obj
+
+    @staticmethod
+    def get_sector_list(df):
+        return df['SECTOR'].unique()
+
 
 if __name__ == '__main__':
     n = Nasdaq100()
-    nasdaq_100_df = n.load_nasdaq_100_data()
-    sectors = nasdaq_100_df['SECTOR'].unique()
-    nasdaq_100_sectors_obj = nasdaq_100_df.groupby('SECTOR')
-    test = nasdaq_100_sectors_obj.get_group('Electronic Technology')
+    sectors_df_list = n.get_nasdaq_100_sectors_df()
     print('debug breakpoint')

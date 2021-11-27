edfd99ba08fbb3dd874f13107ed97373de57e13c
Zayan S. Khan <zayan_k@icloud.com>
2021-11-27 19:51:34 +0000

    DEV current working version

--- app.py
+++ app.py
@@ -19,34 +19,3 @@ app.layout = html.Div(children=
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-# import os
-#
-# import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
-#
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#
-# server = app.server
-#
-# app.layout = html.Div([
-#     html.H2('my app doesnt work so i have this page here instead'),
-#     dcc.Dropdown(
-#         id='dropdown',
-#         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-#         value='LA'
-#     ),
-#     html.Div(id='display-value')
-# ])
-#
-# @app.callback(dash.dependencies.Output('display-value', 'children'),
-#                 [dash.dependencies.Input('dropdown', 'value')])
-# def display_value(value):
-#     return 'You have selected "{}"'.format(value)
-#
-# if __name__ == '__main__':
-#     app.run_server(debug=True)

71a1c3c0cea237f2b8706597c839a059309dd188
Zayan S. Khan <zayan_k@icloud.com>
2021-11-27 15:01:02 +0000

    MIC-18 sectors dict

--- backend/nasdaq_100.py
+++ backend/nasdaq_100.py
@@ -12,11 +12,8 @@ class Nasdaq100:
         nasdaq_100_df = self.load_nasdaq_100_data()
         sector_list = self.get_sector_list(nasdaq_100_df)
         nasdaq_100_obj = self.get_nasdaq_sectors_obj(nasdaq_100_df)
-        sector_df_list = []
-        for sector in sector_list:
-            sector_df = self.get_df_for_sector(nasdaq_100_obj, sector)
-            sector_df_list.append(sector_df)
-        return sector_df_list
+        sectors_df_dict = {sector: self.get_df_for_sector(nasdaq_100_obj, sector) for sector in sector_list}
+        return sectors_df_dict
 
     @staticmethod
     def get_df_for_sector(obj, sector_name):
@@ -34,5 +31,5 @@ class Nasdaq100:
 
 if __name__ == '__main__':
     n = Nasdaq100()
-    sectors_df_list = n.get_nasdaq_100_sectors_df()
+    sectors_dict = n.get_nasdaq_100_sectors_df()
     print('debug breakpoint')

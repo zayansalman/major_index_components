01daa24fe0fb3645319c66faf9719536682eea12
Zayan S. Khan <zayan_k@icloud.com>
2021-11-27 18:05:24 +0000

    testing static app.py

--- app.py
+++ app.py
@@ -1,26 +1,54 @@
-import dash
-from dash import dcc
-from dash import html
-import plotly.express as px
-from backend.nasdaq_100 import Nasdaq100
+# import dash
+# from dash import dcc
+# from dash import html
+# import plotly.express as px
+# from backend.nasdaq_100 import Nasdaq100
+#
+# app = dash.Dash(__name__)
+# server = app.server
+#
+# nasdaq100 = Nasdaq100()
+# sector_list = nasdaq100.get_sector_list(Nasdaq100.load_nasdaq_100_data())
+# nasdaq100_sectors_dict = nasdaq100.get_nasdaq_100_sectors_dict()
+# electronic_technology_df = nasdaq100_sectors_dict.get(sector_list[0])
+#
+# fig = px.bar(electronic_technology_df, x="P/E", y="PRICE", color="TICKER", barmode="group")
+#
+# app.layout = html.Div(children=
+#                       [html.H1(children='Electronic Technology'),
+#                        html.Div(children='''Companies in the Electronic Technology sector of the Nasdaq 100.'''),
+#                        dcc.Graph(id='electronic-technology', figure=fig)])
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
 
-app = dash.Dash(__name__)
+
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
 
-nasdaq100 = Nasdaq100()
-sector_list = nasdaq100.get_sector_list(Nasdaq100.load_nasdaq_100_data())
-nasdaq100_sectors_dict = nasdaq100.get_nasdaq_100_sectors_dict()
-electronic_technology_df = nasdaq100_sectors_dict.get(sector_list[0])
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
 
-fig = px.bar(electronic_technology_df, x="P/E", y="PRICE", color="TICKER", barmode="group")
-
-app.layout = html.Div(children=
-                      [html.H1(children='Electronic Technology'),
-                       html.Div(children='''Companies in the Electronic Technology sector of the Nasdaq 100.'''),
-                       dcc.Graph(id='electronic-technology', figure=fig)])
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+                [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-

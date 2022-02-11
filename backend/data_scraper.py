29e29a05f34dc34db35290bd2becbd782be36f31
Zayan Salman <zayan_k@icloud.com>
2022-02-11 09:55:17 +0000

    MIC-25 dropdown page added but keyerror

--- backend/data_scraper.py
+++ backend/data_scraper.py
@@ -4,13 +4,14 @@ from sources import DB_PATH, NASDAQ_URL, NASDAQ
 
 class DataScraper:
 
-    #ToDo change price column to numeric - split the usd bit away and pd.tonumeric
     @staticmethod
     def get_index_component_data_from_trading_view_as_df(link, index_name):
         index_df = pd.read_html(link)[0]
         index_df.set_axis(['TICKER', 'PRICE', 'CHG', 'PRICE_CHANGE', 'RATING', 'VOLUME',
                            'VOLUME*PRICE', 'MARKET CAP', 'P/E', 'EPS', 'NUMBER OF EMPLOYEES',
                            'SECTOR'], axis=1, inplace=True)
+        index_df['PRICE'] = index_df['PRICE'].str.rstrip('USD')
+        pd.to_numeric(index_df['PRICE'])
         index_df.to_csv(DB_PATH + index_name)
         return index_df
 
